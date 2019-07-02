8c2be569bf6420374fb50265cac2b682ddeb4fe3
Shukurani Isaac <imwakabira@ymail.com>
2019-07-02 12:40:56 +0200

    sol01-d2

--- app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
+++ app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
@@ -4,11 +4,17 @@ import android.content.Context;
 
 import org.hisp.dhis.android.core.d2manager.D2Configuration;
 
+import java.util.Collections;
+
 public class Sdk {
 
     public static D2Configuration getD2Configuration(Context context) {
         // TODO Add configuration
-
-        return null;
+        return D2Configuration.builder()
+                .appName("Dhis2 Skeleton App").appVersion("1.0.0")
+                .context(context)
+                .readTimeoutInSeconds(30).connectTimeoutInSeconds(30).writeTimeoutInSeconds(30)
+                //.networkInterceptors(Collections.singletonList(new StethoInterceptor()))
+                .build();
     }
 }
--- app/src/main/java/com/example/android/androidskeletonapp/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/splash/SplashActivity.java
@@ -3,12 +3,19 @@ package com.example.android.androidskeletonapp.ui.splash;
 import android.os.Bundle;
 
 import com.example.android.androidskeletonapp.R;
+import com.example.android.androidskeletonapp.data.Sdk;
 
 import org.hisp.dhis.android.core.D2;
+import org.hisp.dhis.android.core.d2manager.D2Configuration;
+import org.hisp.dhis.android.core.d2manager.D2Manager;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import io.reactivex.Scheduler;
 import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -26,7 +33,7 @@ public class SplashActivity extends AppCompatActivity {
                     }
                 ).doOnError(throwable -> {
                     // TODO Toast error
-                })
+                }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
                 .subscribe();
     }
 
@@ -40,6 +47,8 @@ public class SplashActivity extends AppCompatActivity {
 
     private Single<D2> instantiateD2() {
         // TODO Instantiate d2
-        return Single.never();
+        return D2Manager.setUp(Sdk.getD2Configuration(getBaseContext()))
+                .andThen(D2Manager.setServerUrl("http://android2.dhis2.org:8080"))
+                .andThen(D2Manager.instantiateD2());
     }
 }
