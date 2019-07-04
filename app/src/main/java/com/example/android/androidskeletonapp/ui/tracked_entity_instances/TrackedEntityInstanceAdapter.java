a839f14158c058bb233fbedbc53f92373ef259cc
Shukurani Isaac <imwakabira@ymail.com>
2019-07-04 10:10:16 +0200

    sol014-states

--- app/src/main/java/com/example/android/androidskeletonapp/ui/tracked_entity_instances/TrackedEntityInstanceAdapter.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/tracked_entity_instances/TrackedEntityInstanceAdapter.java
@@ -6,9 +6,11 @@ import android.view.ViewGroup;
 
 import com.example.android.androidskeletonapp.R;
 import com.example.android.androidskeletonapp.data.service.DateFormatHelper;
+import com.example.android.androidskeletonapp.data.service.StyleBinderHelper;
 import com.example.android.androidskeletonapp.ui.base.DiffByIdItemCallback;
 import com.example.android.androidskeletonapp.ui.base.ListItemWithSyncHolder;
 
+import org.hisp.dhis.android.core.common.State;
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityAttributeValue;
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstance;
 
@@ -45,12 +47,13 @@ public class TrackedEntityInstanceAdapter extends PagedListAdapter<TrackedEntity
         holder.title.setText(valueAt(values, attributePatientNameUid()));
         holder.subtitle1.setText(valueAt(values, attributePatientIdUid()));
         holder.subtitle2.setText(setSubtitle2(values));
-        holder.rightText.setText(DateFormatHelper.formatDate(trackedEntityInstance.created()));
+        //holder.rightText.setText(DateFormatHelper.formatDate(trackedEntityInstance.created()));
         holder.icon.setImageResource(R.drawable.ic_person_black_24dp);
         setBackgroundColor(R.color.colorAccentDark, holder.icon);
 
         // TODO show the correct syncIcon for each tracked entity instance state
         // TODO - setState(trackedEntityInstance.state(), holder.syncIcon);
+        StyleBinderHelper.setState(trackedEntityInstance.state(), holder.syncIcon);
     }
 
     private String valueAt(List<TrackedEntityAttributeValue> values, String attributeUid) {
